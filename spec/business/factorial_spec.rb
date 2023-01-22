e82b67a078879824617ce90da5e7cf41d9081cbd
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-01-21 23:51:50 -0300

    refactor: inclusion of variable n

--- spec/business/factorial_spec.rb
+++ spec/business/factorial_spec.rb
@@ -3,19 +3,22 @@ require 'rails_helper'
 RSpec.describe 'Calcula fatorial de um número' do
   it '1: Se n < 0 deve retornar nil' do
     test = Factorial.new
-    factorial = test.factorial(-5)
+    n = -5
+    factorial = test.factorial(n)
     expect(factorial).to eq(nil)
   end
 
   it '2: Se n = 0 deve retornar 1' do
     test = Factorial.new
-    factorial = test.factorial(0)
+    n = 0
+    factorial = test.factorial(n)
     expect(factorial).to eq(1)
   end
 
   it '3: Se n > 0 deve retornar n!' do
     test = Factorial.new
-    factorial = test.factorial(5)
+    n = 5
+    factorial = test.factorial(n)
     expect(factorial).to eq(120)
   end
 end
