ebaf1ee2c8abe555bcfdaf5293d46bf3dc94a3fb
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-04-22 22:37:32 -0300

    feat: create table job_titles

--- query_sql/query_sql.sql
+++ query_sql/query_sql.sql
@@ -10,3 +10,12 @@ CREATE TABLE teams
 INSERT INTO teams (name) VALUES ('TI');
 INSERT INTO teams (name) VALUES ('Suporte');
 INSERT INTO teams (name) VALUES ('Financeiro');
+
+CREATE TABLE job_titles
+(
+    id int unsigned not null auto_increment,
+    name varchar(40) not null,
+    PRIMARY KEY (id)
+);
+
+
