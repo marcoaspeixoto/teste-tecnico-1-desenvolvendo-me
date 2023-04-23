5cb1b8b6b74e05f9840943f08c442b32932a201c
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-04-22 22:33:25 -0300

    feat: create table teams

--- query_sql/query_sql.sql
+++ query_sql/query_sql.sql
@@ -1,8 +1,8 @@
+CREATE DATABASE human_resources;
 
 CREATE TABLE teams
 (
-    id int unsigne not null,
+    id int unsigned not null auto_increment,
     name varchar(40) not null,
     PRIMARY KEY (id)
 );
-
