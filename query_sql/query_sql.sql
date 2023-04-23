f72ff19b4eae53a700b6e1d56b0ac35bc8cd75a7
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-04-22 23:03:00 -0300

    feat: create table employees

--- query_sql/query_sql.sql
+++ query_sql/query_sql.sql
@@ -23,3 +23,16 @@ INSERT INTO job_titles (name) VALUES ('Desenvolvedor front-end');
 INSERT INTO job_titles (name) VALUES ('Encantador de clientes');
 
 SELECT * FROM teams;
+
+CREATE TABLE employees
+(
+    id int unsigned not null auto_increment,
+    first_name varchar(40) not null,
+    last_name varchar(40) not null,
+    cpf char(11) not null,
+    team_id int unsigned,
+    job_title_id int unsigned,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_employees_teams FOREIGN KEY (team_id) REFERENCES teams (id),
+    CONSTRAINT fk_employees_job_titles FOREIGN KEY (job_title_id) REFERENCES job_titles (id)
+);
